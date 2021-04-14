fix covariance bug

CUB_200_2011/data_processing.py
@@ -19,10 +19,12 @@ class Processer:
         self.num_captions_per_image = metadata['num_captions_per_image']
         self.imgs = torch.load(data_path)
 
-    def process(self):
+    def process(self, size=None):
         captions = []
         images = []
-        for idx in range(len(self.imgs)):
+        if size is None:
+            size = len(self.imgs)
+        for idx in range(size):
             print(idx)
             img = self.imgs[idx]
             img_id = self.img_ids[idx]

CUB_200_2011/execute.py
@@ -1,13 +1,24 @@
 from corr_LDA import M_step_Vectorization
 import numpy as np
+from sklearn import preprocessing
+import joblib
 
-path = ''
+
+print("loading...")
+path = '/Users/kouakiradou/Machine Learning/Final-Year-project/Corr-LDA/CUB_200_2011/preprocessed_data/processed_data.npz'
 data = np.load(path, allow_pickle=True)
 
 images = data['images']
 captions = data['captions']
 
-alpha, BETA, Mean, Covariances, PHI, LAMBDA, GAMMA = M_step_Vectorization(images=images, captions=captions,k=10,tol=1e-3,tol_estep=1e-3,max_iter=100,initial_alpha_shape=100,initial_alpha_scale=0.01)
+print("scaling...")
+scaler = preprocessing.MinMaxScaler()
+scaler.fit(np.vstack(images))
+new_image = [scaler.transform(image) for image in images]
+joblib.dump(scaler, ".pkl")
 
-result_parameter = ''
+print("start training")
+alpha, BETA, Mean, Covariances, PHI, LAMBDA, GAMMA = M_step_Vectorization(images=new_image, captions=captions,k=10,tol=1e-3,tol_estep=1e-3,max_iter=100,initial_alpha_shape=100,initial_alpha_scale=0.01)
+
+result_parameter = '/Users/kouakiradou/Machine Learning/Final-Year-project/Corr-LDA/CUB_200_2011/preprocessed_data/result.npz'
 np.savez(result_parameter, alpha=alpha, BETA=BETA, Mean=Mean, Covariances=Covariances, PHI=PHI, LAMBDA=LAMBDA, GAMMA=GAMMA)

CUB_200_2011/test.py
@@ -1,22 +1,82 @@
 from CUB_200_2011.data_processing import Processer
 import numpy as np
+from sklearn import preprocessing
+from sklearn.datasets import make_spd_matrix
 imgs_path = '/Users/kouakiradou/Machine Learning/Final-Year-project/Corr-LDA/dataset/archive/imgs_train_val_64x64.pth'
 metadata_path = '/Users/kouakiradou/Machine Learning/Final-Year-project/Corr-LDA/dataset/archive/metadata.pth'
-
+result_parameter = '/Users/kouakiradou/Machine Learning/Final-Year-project/Corr-LDA/CUB_200_2011/preprocessed_data/result.npz'
 
 processed_data_path = '/Users/kouakiradou/Machine Learning/Final-Year-project/Corr-LDA/CUB_200_2011/preprocessed_data/processed_data.npz'
 # captionsPath = '/Users/kouakiradou/Machine Learning/Final-Year-project/Corr-LDA/CUB_200_2011/preprocessed_data/'
 
 processor = Processer(imgs_path, metadata_path)
-images, captions = processor.process()
-# print(images, [caption.shape for caption in captions])
+images, captions = processor.process(size=3)
+#
+# # w = np.empty((0,19))
+# # c = np.ones((1,19))
+# scalar = preprocessing.StandardScaler()
+# scalar.fit(np.vstack(images))
+# new_image = [scalar.transform(image) for image in images]
 np.savez(processed_data_path, images=images, captions=captions)
+# # for
 
-data = np.load(processed_data_path, allow_pickle=True)
-new_img = data['images']
-new_cap = data['captions']
-print([image.shape for image in new_img])
-print([caption.shape for caption in new_cap])
-print(type(new_img))
-print(type(new_cap))
-print(type(new_img[0]))
+# b = np.ones((3,3))
+# print(b)
+# a = np.identity(b.shape[0]) * 0.01
+# print(a+b)
+# c = make_spd_matrix(3)
+# print(c)
+
+
+
+
+# print(images[0])
+# scalar = preprocessing.StandardScaler()
+# images_data = [scalar.fit_transform(X) for X in images]
+# # print(images)
+# temp = np.asarray(images)
+# print(temp.shape)
+# new_img = temp.reshape(-1, temp.shape[-1])
+# # print(new_img)
+# print
+# print(scalar.mean_)
+# # print(images, [caption.shape for caption in captions])
+# np.savez(processed_data_path, images=images, captions=captions)
+
+# data = np.load(result_parameter, allow_pickle=True)
+# print(data['LAMBDA'])
+
+# Covariances = [np.zeros((5, 5)) for i in range(3)]
+# print(Covariances)
+# Covariances[0][0][0] = 1
+# print(Covariances)
+
+# mat = [np.zeros((3,3)) for _ in range(5)]
+# mat[0] += np.ones((3,3))
+# print(mat)
+# data = np.load(processed_data_path, allow_pickle=True)
+# new_img = data['images']
+# new_cap = data['captions']
+# print(new_img[0])
+# print([image.shape for image in new_img])
+# print([caption.shape for caption in new_cap])
+# print(type(new_img))
+# print(type(new_cap))
+# print(type(new_img[0]))
+
+
+# scalar = preprocessing.StandardScaler()
+# new_images = np.asarray(images)
+# print(type(images))
+# print(type(new_images))
+# a = new_images.reshape(-1, new_images.shape[-1])
+# print(a)
+# scalar.fit(new_images.reshape(-1, new_images.shape[-1]))
+# scaled_images = [scalar.transform(img) for img in images]
+# print(scaled_images)
+
+
+# for i in range(k):
+#     Covariances[i] += np.identity(dim) * 0.001
+#
+# Covariances0.append(make_spd_matrix(dim))

corr_LDA.py
@@ -5,6 +5,7 @@ import numpy as np
 from numpy import matlib
 from scipy.special import digamma, polygamma
 from scipy.stats import multivariate_normal
+from sklearn.datasets import make_spd_matrix
 import matplotlib.pyplot as plt
 import matplotlib
 matplotlib.use('TkAgg')
@@ -130,7 +131,8 @@ def E_step_Vectorization(alpha, BETA, Mean, Covariances, image, caption, Phi0, g
         #     print(dia_gamma)
         # log_likelihood = np.exp(Lambdaa.T @ np.log(caption @ BETA.T))
         Phi = multivari_pdf * np.exp(digamma(gamma) - digamma(sum(gamma))) * np.exp(Lambdaa.T @ np.log(caption @ BETA.T)) #please double check
-
+        # dia_gamma = np.exp(digamma(gamma) - digamma(sum(gamma)))
+        # expppp = np.exp(Lambdaa.T @ np.log(caption @ BETA.T))
         # exp = np.exp(Lambdaa.T @ np.log(caption @ BETA.T))
         # diag = np.exp(digamma(gamma) - digamma(sum(gamma)))
         # print("-----Phi-------")
@@ -223,8 +225,10 @@ def  M_step_Vectorization(images, captions, k, tol=1e-3, tol_estep=1e-3, max_ite
 
     Covariances0 = []
     for i in range(k):
-        temp_mat = np.random.rand(dim, dim)
-        Covariances0.append(temp_mat @ temp_mat.T)
+        # temp_mat = np.random.rand(dim, dim)
+        # Covariances0.append(temp_mat @ temp_mat.T)
+        # Covariances0.append(make_spd_matrix(dim))
+        Covariances0.append(np.eye(dim))
 
     PHI = [np.ones((N[d], k)) / k for d in range(D)]
     LAMBDA = [np.ones((M[d], N[d])) / N[d] for d in range(D)]
@@ -261,14 +265,15 @@ def  M_step_Vectorization(images, captions, k, tol=1e-3, tol_estep=1e-3, max_ite
 
         #update mean and covariance
         Mean = np.mat(np.zeros((k, dim)))
-        Covariances = [np.zeros((dim, dim))] * k
+        # Covariances = [np.zeros((dim, dim))] * k # that is a fucking big error, remember this create k shallow copy of matrix
+        Covariances = [np.zeros((dim, dim)) for _ in range(k)]
         for d in range(D):
             for i in range(k):
                 Mean[i, :] += np.sum(np.multiply(images[d], np.mat(PHI[d][:, i]).T), axis=0) / Nk[i]
                 cov_k = (images[d] - Mean[i]).T * np.multiply((images[d] - Mean[i]), np.mat(PHI[d][:, i]).T) / Nk[i]
                 Covariances[i] += cov_k
-
-        # print(Mean)
+        for i in range(k):
+            Covariances[i] += np.identity(dim) * 0.001
         alpha_dis = np.mean((alpha - alpha0) ** 2)
         beta_dis = np.mean((BETA - BETA0) ** 2)
         alpha0 = alpha

synthetic_data/synthetic_test.py
@@ -5,6 +5,8 @@ import heapq
 from skimage.util import img_as_float
 from skimage import io
 from corr_LDA import M_step_Vectorization
+from sklearn import preprocessing
+import numpy as np
 
 print("generating synthetic data...")
 origin_images, origin_captions = make_synthetic(500)  # origin_captions be like ['Green','BLUE', 'YELLOW']
@@ -27,16 +29,27 @@ images = image_process(image_from_file)
 captions, dictionary = caption_process(origin_captions)
 print(dictionary.token2id)
 
+print("scaling...")
+scalar = preprocessing.MinMaxScaler()
+scalar.fit(np.vstack(images))
+new_image = [scalar.transform(image) for image in images]
+print(images[0], images[1])
+print(new_image[0],new_image[1])
+
 print("training...")
-alpha_est, beta_est, Mean_est, phi, lambdaa = M_step_Vectorization(images=images, captions=captions,k=2,tol=1e-3,tol_estep=1e-3,max_iter=100,initial_alpha_shape=100,initial_alpha_scale=0.01)
+alpha, BETA, Mean, Covariances, PHI, LAMBDA, GAMMA = M_step_Vectorization(images=new_image, captions=captions,k=2,tol=1e-3,tol_estep=1e-3,max_iter=100,initial_alpha_shape=100,initial_alpha_scale=0.01)
 print("beta matrix------------")
-print(beta_est)
+print(BETA)
 # print("phi--------------------")
-# print(phi)
+# print(PHI)
 # print("lambda-----------------")
-# print(lambdaa)
+# print(LAMBDA)
+# print("gamma-----------------")
+# print(GAMMA)
 print("mean-----------------")
-print(Mean_est)
+print(Mean)
+# print("variance------------")
+# print(Covariances)
 
 
 def find_index(x):
@@ -50,7 +63,7 @@ def find_index(x):
 
     return index
 
-rep_words_index = list(map(find_index, beta_est))
+rep_words_index = list(map(find_index, BETA))
 print([dictionary[i] for i in rep_words_index[0]])
 print([dictionary[i] for i in rep_words_index[1]])
 